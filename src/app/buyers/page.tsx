7d1729fd2bb744b562a7c42c9a8c0967c7ecdb50
codey08 <krishsharda18@gmail.com>
2025-09-26 00:42:55 +0530

    Fix Next.js 15 searchParams type for deployment

--- src/app/buyers/page.tsx
+++ src/app/buyers/page.tsx
@@ -6,7 +6,7 @@ import { redirect } from 'next/navigation';
 export default async function BuyersPage({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const session = await auth();
   
@@ -14,10 +14,12 @@ export default async function BuyersPage({
     redirect('/auth/signin');
   }
 
+  const resolvedSearchParams = await searchParams;
+
   return (
     <MainLayout>
       <div className="px-4 sm:px-6 lg:px-8">
-        <BuyersList searchParams={searchParams} />
+        <BuyersList searchParams={resolvedSearchParams} />
       </div>
     </MainLayout>
   );
